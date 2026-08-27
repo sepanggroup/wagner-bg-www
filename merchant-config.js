ed4fe82c7b567ba2e75fcfb7017a9433fa0655ef
sepanggroup <sepanggroupltd@gmail.com>
2026-08-27 14:31:48 +0300

    feat: configure SEPANG GROUP merchant

---
+++ merchant-config.js
@@ -0,0 +1,9 @@
+export const MERCHANT = {
+  legalName: 'SEPANG GROUP ЕООД',
+  brand: 'WAGNER-BG',
+  email: 'sepanggroupltd@gmail.com',
+  website: 'https://www.wagner-bg.shop/'
+};
+
+// Public PayPal Client ID. Never place a PayPal Client Secret in frontend code.
+export const PAYPAL_CLIENT_ID = 'BAAwAOX8YivoshgObV1N0aA4bR9FIueXriBgZtXSzkmpAc0itgZ50Wuon3gnhAV-uPJWeKhNGCIS87S768';
