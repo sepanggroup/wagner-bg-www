3da1a420a3a286076ad25e83f011ea14e6712b58
sepanggroup <sepanggroupltd@gmail.com>
2026-08-27 14:58:33 +0300

    catalog: add curated unique robot product photography map

---
+++ image-overrides.js
@@ -0,0 +1,13 @@
+export const PRODUCT_IMAGE_OVERRIDES = {
+  'partner-p900': 'https://console.kr-asia.com/wp-content/uploads/2025/11/Partner-Robotics-P900.jpg',
+  'fangshi-ceramic-tile-robot': 'https://en.fangshitech.com/upload/image/20260522/2195f5ee047701b4e5bd15f4abe55677.png',
+  'fangshi-stone-tile-robot': 'https://en.fangshitech.com/upload/image/20260522/a9e5e36fc56df335f1905eebc7354f05.png',
+  'zhuling-tlr': 'https://32786180.s21i.faiusr.com/4/ABUIABAEGAAg3OCRuQYowuvHxAMwtgM4pgM.png',
+  'bright-dream-r19': 'https://www.red-dot.org/fileadmin/_processed_/d/5/csm_35-06495-2022PD-02_CO_EV_fa64447f31.jpg',
+  'smartbuild-thinset-robot': 'https://s.alicdn.com/%40sc04/kf/H06cc4adfc42946918eb7c270df9593e6w/Automatic-Tile-Installation-Robot-Labor-saving-Floor-Tile-Laying-Machine-Smart-Construction-Robot-for-Tile-Paving.jpg',
+  'derutu-tile-laying-robot': 'https://www.derutu.com.cn/uploads/allimg/20240727/1-240HG03625a5.jpg',
+  'yanling-tile-robot': 'https://s.alicdn.com/%40sc04/kf/Hb6ea0d65e1c8449d849ea9a2b8c3cf39Q.jpg_640x640.jpg',
+  'kaifeng-yucheng-automatic-tile-robot': 'https://s.alicdn.com/%40sc04/kf/Haf5de4b95e4b43f99976ef313dd62789t.jpg',
+  'bossgoo-intelligent-tile-robot': 'https://s.alicdn.com/%40sc04/kf/H2d24226b3e6446c5bf54b466f8c5bdc6j/Tile-Automatic-Laying-Robot-High-Precision-Tile-Installation-Robot-High-Efficiency-for-Commercial-Buildings.jpg',
+  'bossgoo-palletizer-tile-robot': 'https://s.alicdn.com/%40sc04/kf/H55b621068c58427ba48bfa9c7adb842ag.png_640x640.png'
+};
