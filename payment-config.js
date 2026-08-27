2fa755e69fdb57f88afdbb98b4b6579a47fd0148
sepanggroup <sepanggroupltd@gmail.com>
2026-08-27 11:08:43 +0300

    chore: add isolated WAGNER PayPal configuration

---
+++ payment-config.js
@@ -0,0 +1,3 @@
+// Public PayPal client configuration for this storefront.
+// Keep this value empty until the PayPal Business application belonging to КОЛМАН ЕООД is confirmed.
+export const PAYPAL_CLIENT_ID = '';
